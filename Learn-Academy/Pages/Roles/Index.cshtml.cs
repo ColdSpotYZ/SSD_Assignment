69e3d3efa0bb37c53310016a0e71db01248f2dbd
Kai Xian Lim <s10195144@connect.np.edu.sg>
2020-07-23 22:34:41 +0800

    Created new pages for for authorization

---
+++ Learn-Academy/Pages/Roles/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Learn_Academy.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Learn_Academy.Pages.Roles
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public IndexModel(RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public List<ApplicationRole> ApplicationRole { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Get a list of roles
+            ApplicationRole = await _roleManager.Roles.ToListAsync();
+
+        }
+
+    }
+}
