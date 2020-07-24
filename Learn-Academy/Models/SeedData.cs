10a1ab2d2da30a17e547a4e77930934118e10507
Kai Xian Lim <s10195144@connect.np.edu.sg>
2020-07-24 16:46:19 +0800

    Added scrollbar for Courses

--- Learn-Academy/Models/SeedData.cs
+++ Learn-Academy/Models/SeedData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
 
 namespace Learn_Academy.Models
 {
@@ -14,7 +15,7 @@ namespace Learn_Academy.Models
                 serviceProvider.GetRequiredService<
                     DbContextOptions<Learn_AcademyContext>>()))
             {
-                // Look for any movies.
+                // Look for any Courses.
                 if (context.Course.Any())
                 {
                     return;   // DB has been seeded
@@ -53,6 +54,55 @@ namespace Learn_Academy.Models
                         Price = 189.99M
                     }
                 );
+                // // Look for any users.
+                // if (context.Users.Any())
+                // {
+                //     return;   // DB has been seeded
+                // }
+
+                // context.Users.AddRange(
+                //     new IdentityUser
+                //     {
+                //         Id = "a24 - ef927587ba68",
+                //         UserName = "admin2@gmail.com",
+                //         NormalizedUserName = "ADMIN2@GMAIL.COM",
+                //         Email = "admin2@gmail.com",
+                //         NormalizedEmail = "ADMIN2@GMAIL.COM",
+                //         PasswordHash = "AQAAAAEAACcQAAAAEKkzN9HbiD2j6cnpQDFMkhmnzk0CjpLGie9yHpTbtatUxbFH7nNG3guYvuccGFqhrA==",
+                //         SecurityStamp = "BYNQ2V4LVRAZERYR73FC2RK7UOLGQEOC",
+                //         ConcurrencyStamp = "c3e12195-2fdb-4b1d-8485-60d11dbebbc5",
+                //         PhoneNumberConfirmed = false,
+                //         PhoneNumber = null,
+                //         TwoFactorEnabled = null,
+                //         LockoutEnd = null,
+                //         LockoutEnabled = true,
+                //         FullName = 
+                //     },
+
+                //     new Course
+                //     {
+                //         Name = "Wallet Making",
+                //         CourseDate = DateTime.Parse("2020-9-13"),
+                //         Type = "On-Site",
+                //         Price = 179.99M
+                //     },
+
+                //     new Course
+                //     {
+                //         Name = "Knitting",
+                //         CourseDate = DateTime.Parse("2020-9-13"),
+                //         Type = "Online",
+                //         Price = 49.99M
+                //     },
+
+                //     new Course
+                //     {
+                //         Name = "Deep Learning",
+                //         CourseDate = DateTime.Parse("2020-9-15"),
+                //         Type = "Online",
+                //         Price = 189.99M
+                //     }
+                // );
                 context.SaveChanges();
             }
         }
