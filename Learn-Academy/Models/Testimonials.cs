7cb6335833516754b76c5f61e17e286171d411d3
Kai Xian Lim <s10195144@connect.np.edu.sg>
2020-08-04 17:14:53 +0800

    Updated classes

--- Learn-Academy/Models/Testimonials.cs
+++ Learn-Academy/Models/Testimonials.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Policy;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Learn_Academy.Models
     public class Testimonials
     {
         public int ID { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Details { get; set; }
     }
 }
